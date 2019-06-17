872189ed34a634ef94b121945e79afcbcf0732c9
Shubham Tripathi <tripathi.shubham509@gmail.com>
2019-06-17 11:08:33 +0530

    hotelresults activity created

---
+++ app/src/main/java/com/iniesta/darshan/Activities/HotelResultsActivity.java
@@ -0,0 +1,15 @@
+package com.iniesta.darshan.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iniesta.darshan.R;
+
+public class HotelResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel_results);
+    }
+}
