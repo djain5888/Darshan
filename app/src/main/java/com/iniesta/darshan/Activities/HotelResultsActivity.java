dfdbd272601f7cf4fe10095eed4377dbfea3e66b
Ranger12021999 <amansharma123a@gmail.com>
2019-06-18 14:19:45 +0530

    Dialog box with Image Slider

--- app/src/main/java/com/iniesta/darshan/Activities/HotelResultsActivity.java
+++ app/src/main/java/com/iniesta/darshan/Activities/HotelResultsActivity.java
@@ -1,11 +1,15 @@
 package com.iniesta.darshan.Activities;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +19,8 @@ import com.iniesta.darshan.models.HotelModel;
 import com.iniesta.darshan.models.poojamodel;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HotelResultsActivity extends AppCompatActivity {
 
@@ -26,6 +32,12 @@ public class HotelResultsActivity extends AppCompatActivity {
     private HotelAdapter.RecyclerViewClickListener listener;
 
     private ArrayList<HotelModel> hotelList = new ArrayList<>();
+
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static int NUM_PAGES = 0;
+    private static final Integer[] IMAGES= {R.drawable.varansismall,R.drawable.mathura,R.drawable.vrindawan,R.drawable.haridwar};
+    private ArrayList<Integer> ImagesArray = new ArrayList<Integer>();
 
 
     @Override
@@ -39,10 +51,36 @@ public class HotelResultsActivity extends AppCompatActivity {
         checkInTextView = findViewById(R.id.checkInText);
         checkOutTextView = findViewById(R.id.checkOutText);
 
+        for(int i=0;i<IMAGES.length;i++)
+            ImagesArray.add(IMAGES[i]);
+
         listener = new HotelAdapter.RecyclerViewClickListener() {
             @Override
             public void onClick(View view, int position) {
                 Toast.makeText(getApplicationContext(),position + " clicked",Toast.LENGTH_SHORT).show();
+                final Dialog dialog = new Dialog(HotelResultsActivity.this);
+                dialog.setContentView(R.layout.hotel_image_dialog);
+                mPager= dialog.findViewById(R.id.pager);
+                mPager.setAdapter(new HotelPagerAdapter(HotelResultsActivity.this,ImagesArray));
+                // Auto start of viewpager
+//                final Handler handler = new Handler();
+//                final Runnable Update = new Runnable() {
+//                    public void run() {
+//                        if (currentPage == NUM_PAGES) {
+//                            currentPage = 0;
+//                        }
+//                        mPager.setCurrentItem(currentPage++, true);
+//                    }
+//                };
+//                Timer swipeTimer = new Timer();
+//                swipeTimer.schedule(new TimerTask() {
+//                    @Override
+//                    public void run() {
+//                        handler.post(Update);
+//                    }
+//                }, 1500, 1500);
+
+                dialog.show();
             }
         };
 
