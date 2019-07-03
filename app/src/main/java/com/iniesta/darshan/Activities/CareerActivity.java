84bffcb3fd3e51f6e46c150d19998fdb4bd7d75f
Shubham Tripathi <tripathi.shubham509@gmail.com>
2019-07-03 15:38:45 +0530

    Added careers activity

---
+++ app/src/main/java/com/iniesta/darshan/Activities/CareerActivity.java
@@ -0,0 +1,26 @@
+package com.iniesta.darshan.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.iniesta.darshan.R;
+
+public class CareerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_career);
+
+
+    }
+
+    public void applyCareers(View view){
+        //Intent intent = new Intent(CareerActivity.this, Applynow.class);
+        //startActivity(intent);
+        Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show();
+    }
+}
