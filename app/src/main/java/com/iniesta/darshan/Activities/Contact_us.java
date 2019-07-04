68090dc90138f49828994d7019493f57290c5ab4
Admin <ujjwalchikara570@gmail.com>
2019-07-04 10:41:20 +0530

    apply now and contact us

--- app/src/main/java/com/iniesta/darshan/Activities/Contact_us.java
+++ app/src/main/java/com/iniesta/darshan/Activities/Contact_us.java
@@ -1,17 +1,16 @@
 package com.iniesta.darshan.Activities;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.iniesta.darshan.R;
 
-public class About_us extends AppCompatActivity {
+public class Contact_us extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
+        setContentView(R.layout.activity_contact_us);
     }
 }
 
